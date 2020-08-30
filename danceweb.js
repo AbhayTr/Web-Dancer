c9b5a6332d29aaf32f5f6f71bfdb9b4abf0e0741
AbhayTr <53339132+AbhayTr@users.noreply.github.com>
2020-08-30 22:22:12 +0530

    Add files via upload

--- danceweb.js
+++ danceweb.js
@@ -1,10 +1,16 @@
+//For Non-Phone Devices
+
 a = 0;
- b = document.getElementsByTagName("div");
- setInterval(function()
- {
-   for (i = 0; i < b.length; i++)
-   {
-     b[i].style.transform = "rotate(" + a + "deg)";
-   }
-   a += 1;
- }, 20);
+b = document.getElementsByTagName("div");
+setInterval(function()
+{
+  for (i = 0; i < b.length; i++)
+  {
+    b[i].style.transform = "rotate(" + a + "deg)";
+  }
+  a += 1;
+}, 20);
+
+//For Phone Devices (Paste the below code in the URL bar after opening your desired website)
+
+javascript:a=0;b = document.getElementsByTagName("div");setInterval(function(){for(i=0;i<b.length;i++){b[i].style.transform = "rotate(" + a + "deg)";}a += 1;}, 20);
