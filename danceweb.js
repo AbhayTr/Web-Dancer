e9ca46a421f0afa5d6ca56fa05fd27c4c3655de5
AbhayTr <53339132+AbhayTr@users.noreply.github.com>
2020-08-30 22:38:08 +0530

    Add files via upload

--- danceweb.js
+++ danceweb.js
@@ -1,1 +1,10 @@
-a=0;b = document.getElementsByTagName("div");setInterval(function(){for(i=0;i<b.length;i++){b[i].style.transform = "rotate(" + a + "deg)";}a += 1;}, 20);
+a = 0; 						       //Angle to rotate in degrees
+b = document.getElementsByTagName("div");	       //List of all <div> elements
+setInterval(function()				       //Make them dance!
+{
+  for (i = 0; i < b.length; i++)		       //Iterate through all <div> elements
+  {
+    b[i].style.transform = "rotate(" + a + "deg)";     //Setting the css property to rotate
+  }
+  a += 1;					       //Incrementing the angle 
+}, 20);						       //Makes webpage dance endlessly
