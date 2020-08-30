ebce3cf6d5f6262d3746573ecf92f83e77b91721
AbhayTr <53339132+AbhayTr@users.noreply.github.com>
2020-08-30 22:23:45 +0530

    Add files via upload

--- danceweb.js
+++ danceweb.js
@@ -13,4 +13,4 @@ setInterval(function()
 
 //For Phone Devices (Paste the below code in the URL bar after opening your desired website)
 
-javascript:a=0;b = document.getElementsByTagName("div");setInterval(function(){for(i=0;i<b.length;i++){b[i].style.transform = "rotate(" + a + "deg)";}a += 1;}, 20);
+javascript: a=0;b = document.getElementsByTagName("div");setInterval(function(){for(i=0;i<b.length;i++){b[i].style.transform = "rotate(" + a + "deg)";}a += 1;}, 20);
