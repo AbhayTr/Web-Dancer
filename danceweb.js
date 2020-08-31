a86817140b8ce6f4026708135ace57959cb19724
AbhayTr <53339132+AbhayTr@users.noreply.github.com>
2020-08-31 13:09:57 +0530

    Add files via upload

--- danceweb.js
+++ danceweb.js
@@ -1,10 +1,10 @@
-a = 0; 						       //Angle to rotate in degrees
-b = document.getElementsByTagName("div");	       //List of all <div> elements
-setInterval(function()				       //Make them dance!
+a = 0; 						       //Angle to rotate in degrees.
+b = document.getElementsByTagName("div");	       //List of all <div> tag elements.
+setInterval(function()				       //Makes the webpage dance!
 {
-  for (i = 0; i < b.length; i++)		       //Iterate through all <div> elements
+  for (i = 0; i < b.length; i++)		       //Iterate through all <div> tag elements.
   {
-    b[i].style.transform = "rotate(" + a + "deg)";     //Setting the css property to rotate
+    b[i].style.transform = "rotate(" + a + "deg)";     //Setting the css property to rotate each <div> tag element on random angles.
+    a = Math.floor(Math.random() * 361);               //Setting the random angle for next element for dancing effect!
   }
-  a = Math.floor(Math.random() * 361);                 //Setting random angle for effect 
-}, 70);						       //Makes webpage dance endlessly
+}, 100);					       //Makes webpage dance endlessly by randomly rotating all <div> tag elements
